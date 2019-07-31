9f21186be485755ce8d9a5ccb526aee7397962cf
Rafael Ruiz <raf.ruiz.moreno@gmail.com>
2019-07-31 23:27:56 +0200

    Use elixir dates instead of strings

--- apps/flights_aggregator/lib/flights_aggregator.ex
+++ apps/flights_aggregator/lib/flights_aggregator.ex
@@ -13,12 +13,13 @@ defmodule FlightsAggregator do
 
   ## Examples
 
-      iex> FlightsAggregator.find_combined_flights(~w(SZZ SXF), "01/09/2019")
+      iex> FlightsAggregator.find_combined_flights(~w(SZZ SXF), ~D[2019-08-01])
       [
         %FlightsAggregator.CombinedFlights{
           destination: "Oslo",
           flights: [ .. ],
-          origins: [SZZ, SXF]
+          origins: [SZZ, SXF],
+          departure_date: ~D[2019-08-01]
         }
       ]
 
@@ -28,22 +29,40 @@ defmodule FlightsAggregator do
     |> Enum.flat_map(&(fetch_flights(&1, departure_date)))
     |> Enum.group_by(fn flight -> flight["city_to"] end)
     |> Enum.filter(fn {_destination, flights} -> flights |> length == origins |> length end)
-    |> Enum.map(fn {destination, flights} -> %CombinedFlights{origins: origins, destination: destination, flights: flights} end)
+    |> Enum.map(
+         fn {destination, flights} ->
+           %CombinedFlights{
+             origins: origins,
+             destination: destination,
+             flights: flights,
+             departure_date: departure_date
+           }
+         end
+       )
     |> Enum.sort(&((&1 |> CombinedFlights.total_price) < (&2 |> CombinedFlights.total_price) ))
   end
 
   defp fetch_flights(origin, departure_date) do
+    formatted_date = departure_date |> format_date
+
     KiwiApi.Flights.search(
       %{
         fly_from: origin,
         fly_to: "",
-        date_from: departure_date,
-        date_to: departure_date
+        date_from: formatted_date,
+        date_to: formatted_date
       },
       %{
         oneforcity: 1,
         direct_flights: 1
       }
     )
+  end
+
+  defp format_date(date) do
+    [date.day, date.month, date.year]
+    |> Enum.map(&to_string/1)
+    |> Enum.map(&String.pad_leading(&1, 2, "0"))
+    |> Enum.join("/")
   end
 end
--- apps/flights_aggregator/lib/flights_aggregator/combined_flights.ex
+++ apps/flights_aggregator/lib/flights_aggregator/combined_flights.ex
@@ -1,5 +1,5 @@
 defmodule FlightsAggregator.CombinedFlights do
-  defstruct origins: [], destination: nil, flights: []
+  defstruct origins: [], destination: nil, flights: [], departure_date: nil
 
   def total_price(combined_flights) do
     combined_flights.flights |> Enum.reduce(0.0, fn x, acc -> x["price"] + acc end)
